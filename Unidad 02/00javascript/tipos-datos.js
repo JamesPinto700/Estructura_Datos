9b7444b809f937646f89f01b40339cafeb485042
Sebastián P. <james.pinto@unl.edu.ec>
2026-06-12 13:09:40 -0500

    Actualizacion, Avance clase 12/06/2026

---
+++ Unidad 02/00javascript/tipos-datos.js
@@ -0,0 +1,29 @@
+//Datos Primitivos
+
+let nombre = 'Maria'   //Tipo String
+
+let edad = 25   //Tipo Numerico
+
+let esMayorDeEdad = true   //Tipo Booleanos
+
+let noHayValor = null   //Tipo Nulo
+
+let noDefinido = undefined   //Tipo Indefinido
+
+let simboloUnico = Sumbol('único')   //Simbolo de Valor Unico
+
+let numeroGrande = 2n   //Datos Gigantes (big int)
+
+//Datos Completos
+
+//Objects
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+//Arrays
+let frutas = ['manzana', 'pera', 'piña']
+
+//Functions
+function saludar(){ }
