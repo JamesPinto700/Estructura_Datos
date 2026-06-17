d850f35fa989638f5ebd0b51c4d5b5f0e9e93caf
Sebastián P. <james.pinto@unl.edu.ec>
2026-06-17 10:24:58 -0500

    Feature: Tarea del dia 16 de junio, Condicionales y HTML

--- Unidad 02/00javascript/clase-condicional.js
+++ Unidad 02/00javascript/clase-condicional.js
@@ -1,5 +1,44 @@
-/*
-Condicionales
+/*Condicionales*/
 
+//if, else if y else
+function verificarCalificacion(calificacion) {
+    if (calificacion < 7) {
+        return 'Reprobado'
+    } else if (calificacion === 7) {
+        return 'Pasable'
+    } else {
+        return 'Exonerado'
+    }
+}
 
-*/
+//switch
+function verificarDia(dia) {
+    switch (dia) {
+        case 'lunes':
+            return 'Inicio de semana'
+        case 'viernes':
+            return 'Fin de semana laboral'
+        default:
+            return 'Día regular'
+    }
+}
+
+//Operador ternario (Version corta de if '?' y else ':')
+function verificarNota(nota) {
+    const resultado = nota >= 7 ? 'Aprobado' : 'Reprobado'
+    return resultado
+}
+
+//Nullish coalescing '??' (Para tratar valores nulos:
+//Devuelve el lado derecho solo si el izquierdo es nulo o indefinido)
+function verificarEntrada(entrada) {
+    const valor = entrada ?? 'Sin dato'
+    return valor
+}
+
+//Optional chaining '?.' (Se usa para evitar errores al acceder a propiedades que pueden no existir:
+//Devuelve un valor indefinido si no existe un valor)
+function verificarUsuario(usuario) {
+    const nombre = usuario?.nombre
+    return nombre !== undefined ? `Hola, ${nombre}` : 'undefined (el objeto es null)'
+}
