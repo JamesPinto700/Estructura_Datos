a0c9f03be1904a34f303078c010d03d9d9394b45
Sebastián P. <james.pinto@unl.edu.ec>
2026-06-25 11:49:09 -0500

    feat: Laboratorio del APE08 Orientacion Verde. Fecha: 25-06-2026

---
+++ Unidad 02/APE08.Ordenación Verde/RegistroAmbiental.js
@@ -0,0 +1,10 @@
+class RegistroAmbiental {
+
+    constructor (idRegistro,especie,toneladasCO2){
+        this.idRegistro = idRegistro
+        this.especie = especie
+        this.toneladasCO2 = toneladasCO2
+    }
+}
+
+module.exports = RegistroAmbiental;
